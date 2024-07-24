6d3e6310101f49887fc752f7eea8c030297202f6
1159904577 <1159904577@qq.com>
2024-07-24 11:24:30 +0800

    leetcode

---
+++ leetcode_647.java
@@ -0,0 +1,26 @@
+public class leetcode_647 {
+    public static void main(String[] args) {
+
+    }
+
+
+    public int countSubstrings(String s) {
+        int count = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int i = s.length()-1; i >=0 ; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j-i<=1){
+                        count++;
+                        dp[i][j] = true;
+                    }else if(dp[i+1][j-1]){
+                        dp[i][j] = dp[i+1][j-1];
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
